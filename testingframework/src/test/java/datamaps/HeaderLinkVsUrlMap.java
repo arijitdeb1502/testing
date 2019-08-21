75322c42880b3656419b258c9d4e85b5372a0baa
arijitdeb1502 <54313959+arijitdeb1502@users.noreply.github.com>
2019-08-21 15:24:01 +0800

    Add files via upload
    Adding the newly created data maps for events per group page

---
+++ testingframework/src/test/java/datamaps/HeaderLinkVsUrlMap.java
@@ -0,0 +1,19 @@
+package datamaps;
+
+public class HeaderLinkVsUrlMap {
+	
+	private String linkText;
+	private String url;
+	
+	public String getLinkText() {
+		return linkText;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	
+	
+
+}
